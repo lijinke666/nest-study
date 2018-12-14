bf24f1eb69b10bf80389a1cc07ff3179356ffce0
Jinke.Li <1359518268@qq.com>
2018-12-14 11:18:26 +0800

    feat: 新增中间键

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { registerSwagger } from './swagger';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,      // 跨域  内置的 cors 模块
+  });
+  // CSRF 跨站点请求伪造攻击
+  app.use(csurf());
+
+  // 接口限速
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 分钟
+    max: 100, // 最大 100个 ip
+  }));
+
+  // 全局验证
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
   registerSwagger(app)();
+
   await app.listen(3000);
+
+  // 热更新
   acceptHotModule(app);
 }
 
